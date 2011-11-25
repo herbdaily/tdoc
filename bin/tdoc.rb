46c3f19865f24d0cd4e09825b9b399da01dc364e
Herb Daily <herb.daily@safe-mail.net>
2011-11-25 03:20:58 -0300

    context directive now works, more bug fixes

--- bin/tdoc.rb
+++ bin/tdoc.rb
@@ -5,6 +5,7 @@ require 'shoulda'
 
 $: << 'lib'
 
+TEST_DIR=ARGV[0]
 LINST='^[#|\s]*'
 
 def mk_context(file,test_case=nil)
@@ -15,13 +16,14 @@ def mk_context(file,test_case=nil)
   text=File.read(file)
   directives=text.scan(/#{LINST}tdoc_(.+?):\s*(.+)/).inject({}) {|h,d| h[d[0].to_sym]||=[];h[d[0].to_sym] << d[1];h}
   directives[:require].to_a.each {|r| require r}
-  tests=text.split(/#{LINST}[Ee]xamples:/)[1..-1].map do |test|
+  directives[:context].to_a.each {|c| mk_context "#{TEST_DIR}#{c}", test_case}
+  tests=text.split(/#{LINST}[Ee]xamples:/).to_a[1..-1].to_a.map do |test|
     [test.split(/\n/)[0], test.scan(/#{LINST}>>\s*(.+)\n#{LINST}=>\s*(.+)/) ]
   end
   test_case.class_eval do
     context test_name do 
       setup do 
-        eval directives[:setup].join ';'
+        eval directives[:setup].to_a.join ';'
       end
       tests.each do |test|
         should test[0] do
@@ -37,7 +39,7 @@ def mk_tests(test_dir)
   files=Dir.glob("#{test_dir}/*.tdoc")
   files.each { |file| mk_context(file)}
 end
-mk_tests(ARGV[0])
+mk_tests(TEST_DIR)
 
 
 
