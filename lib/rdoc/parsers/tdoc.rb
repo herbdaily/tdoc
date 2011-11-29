0aaaf595622b62d56cf0a0623e0576c114704031
Herb Daily <herb.daily@safe-mail.net>
2011-11-29 19:42:32 -0300

    parser nearly working

--- lib/rdoc/parsers/tdoc.rb
+++ lib/rdoc/parsers/tdoc.rb
@@ -2,11 +2,21 @@ class RDoc::Parser::Tdoc < RDoc::Parser::Simple
   LINST='^[#|\s]*'
   parse_files_matching(/\.rdoc/)
   def initialize(top_level, file_name, content, options, stats)
+    super
+    @content=process_includes(file_name)
+  end
+  def process_includes(file_name)
+    content=File.read(file_name)
     test_name=File.basename(file_name,'.rdoc')
     test_dir=File.dirname(file_name)
-    if File.exists?("#{test_dir}/#{test_name}.rb")
-      examples=content.split()
+    content.gsub!(/#{LINST}:include:\s*(.+)/) {|foo| process_includes $1}
+    Dir.glob("#{test_dir}/#{test_name}.rb").each do |fn|
+      tests=content.split(/#{LINST}[Ee]xamples?:/,2)
+      content="#{tests[0]}#{process_includes(fn)}\nExamples:\n#{tests[1]}"
     end
-    super(top_level, file_name, content, options, stats)
+    Dir.glob("#{test_dir}/#{test_name}/*.rdoc").each do |fn|
+      content+="\n#{process_includes(fn)}\n"
+    end
+    content
   end
 end
