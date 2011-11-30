fdba45a9371f951aeef4b1997979d52a11d37819
Herb Daily <herb.daily@safe-mail.net>
2011-11-30 04:30:06 -0300

    bug fixes

--- lib/rdoc/parsers/tdoc.rb
+++ lib/rdoc/parsers/tdoc.rb
@@ -1,5 +1,6 @@
 class RDoc::Parser::Tdoc < RDoc::Parser::Simple
   LINST='^[#|\s]*'
+  LINSTM='[#|\s]*'
   parse_files_matching(/\.rdoc/)
   def initialize(top_level, file_name, content, options, stats)
     p options.dry_run
@@ -11,6 +12,10 @@ class RDoc::Parser::Tdoc < RDoc::Parser::Simple
     test_name=File.basename(file_name,'.rdoc')
     test_dir=File.dirname(file_name)
     content.gsub!(/(#{LINST}):include:\s*(.+)/) {|foo| indent=$1;process_includes($2).sub(/^/,indent)}
+    content.gsub!(/(#{LINST})setup\s+(.*?)#{LINSTM}end\s+/m) {|foo| 
+      indent=$1
+      "The following code: \n#{$2.sub(/^/,indent+'  ')}\nprecedes each of the following examples.\n\n"
+    }
     Dir.glob("#{test_dir}/#{test_name}.rb").each do |fn|
       tests=content.split(/#{LINST}[Ee]xamples?:/,2)
       content="#{tests[0]}\nThe following examples require the file '#{fn}', whose contents follow:\n#{process_includes(fn).sub(/^/,'  ')}\nExamples: #{tests[1]}"
