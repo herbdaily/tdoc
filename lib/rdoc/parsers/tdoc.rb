a9f69c6cd248c4c2b52ed4c7443dce9770896886
Herb Daily <herb.daily@safe-mail.net>
2011-11-30 05:21:45 -0300

    bug fixes

--- lib/rdoc/parsers/tdoc.rb
+++ lib/rdoc/parsers/tdoc.rb
@@ -3,7 +3,6 @@ class RDoc::Parser::Tdoc < RDoc::Parser::Simple
   LINSTM='[#|\s]*'
   parse_files_matching(/\.rdoc/)
   def initialize(top_level, file_name, content, options, stats)
-    p options.dry_run
     super
     @content=process_includes(file_name)
   end
@@ -12,16 +11,16 @@ class RDoc::Parser::Tdoc < RDoc::Parser::Simple
     test_name=File.basename(file_name,'.rdoc')
     test_dir=File.dirname(file_name)
     content.gsub!(/(#{LINST}):include:\s*(.+)/) {|foo| indent=$1;process_includes($2).sub(/^/,indent)}
-    content.gsub!(/(#{LINST})setup\s+(.*?)#{LINSTM}end\s+/m) {|foo| 
+    content.sub!(/(#{LINST})setup\s+(.*?)#{LINSTM}end\s+/m) {|foo| 
       indent=$1
-      "The following code: \n#{$2.sub(/^/,indent+'  ')}\nprecedes each of the following examples.\n\n"
+      "The following code: \n#{$2.gsub(/^/,indent+'  ')}\nprecedes each of the following examples.\n\n"
     }
     Dir.glob("#{test_dir}/#{test_name}.rb").each do |fn|
       tests=content.split(/#{LINST}[Ee]xamples?:/,2)
-      content="#{tests[0]}\nThe following examples require the file '#{fn}', whose contents follow:\n#{process_includes(fn).sub(/^/,'  ')}\nExamples: #{tests[1]}"
+      content="#{tests[0]}\nThe following examples require the file '#{fn}', whose contents follow:\n#{process_includes(fn).gsub(/^/,'  ')}\nExamples: #{tests[1]}"
     end
     Dir.glob("#{test_dir}/#{test_name}/*.rdoc").each do |fn|
-      content+="\n#{process_includes(fn).sub(/^/,'  ')}\n"
+      content+="\n#{process_includes(fn).gsub(/^/,'  ')}\n"
     end
     content
   end
--- tdoc.gemspec
+++ tdoc.gemspec
@@ -10,7 +10,7 @@ Gem::Specification.new do |s|
   s.homepage    = 'http://github.com/herbdaily/tdoc'
   s.required_rubygems_version = Gem::Requirement.new(">= 1.0.0") if s.respond_to? :required_rubygems_version=
   s.add_runtime_dependency 'shoulda', '~>2.11.3'
-  s.files = Dir.glob(['README.rdoc','bin/*','rdoc/*'])
+  s.files = Dir.glob(['README.rdoc','bin/*','rdoc/*','lib/**/*'])
   s.executables = ["tdoc.rb"]
   s.default_executable = "tdoc.rb"
 end
