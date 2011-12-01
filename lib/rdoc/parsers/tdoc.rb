24f75089c046eae00cb8e412cd8746af432edc07
Herb Daily <herb.daily@safe-mail.net>
2011-11-30 22:15:13 -0300

    temp fix for poor rendering of manually required files

--- lib/rdoc/parsers/tdoc.rb
+++ lib/rdoc/parsers/tdoc.rb
@@ -1,6 +1,7 @@
 class RDoc::Parser::Tdoc < RDoc::Parser::Simple
   LINST='^[#|\s]*'
   LINSTM='[#|\s]*'
+  EXTENSIONS={:tests => '.rdoc',:requires => '.rb'}
   parse_files_matching(/\.rdoc/)
   def initialize(top_level, file_name, content, options, stats)
     super
@@ -10,7 +11,15 @@ class RDoc::Parser::Tdoc < RDoc::Parser::Simple
     content=File.read(file_name)
     test_name=File.basename(file_name,'.rdoc')
     test_dir=File.dirname(file_name)
-    content.gsub!(/(#{LINST}):include:\s*(.+)/) {|foo| indent=$1;process_includes($2).sub(/^/,indent)}
+    content.gsub!(/(#{LINST}):include:\s*(.+)/) do |foo|
+      indent=$1
+      fn=$2
+      if fn.match(/#{EXTENSIONS[:requires]}/)
+        "\nThe following examples require the file '#{fn}', whose contents follow:\n#{process_includes(fn).gsub(/^/,indent + '  ')}\n"
+      else
+        process_includes(fn).gsub(/^/,indent)
+      end
+    end
     content.sub!(/(#{LINST})setup\s+(.*?)#{LINSTM}end\s+/m) {|foo| 
       indent=$1
       "The following code: \n#{$2.gsub(/^/,indent+'  ')}\nprecedes each of the following examples.\n\n"
